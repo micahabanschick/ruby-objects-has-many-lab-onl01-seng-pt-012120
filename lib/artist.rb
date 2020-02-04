7d01d1768f10a9b9813cf250ad3ee4fe2fd10a0c
Micah Banschick <micha.ban@gmail.com>
2020-02-04 20:43:11 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+class Artist 
+  attr_accessor :name, :songs
+  
+  @@song_count = 0
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def add_song(song)
+    song.artist = self
+    @songs << song
+    @@song_count +=1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+    @songs << song
+    @@song_count +=1
+    #binding.pry
+  end
+
+  def songs 
+    @songs 
+    #binding.pry
+  end 
+  
+  def self.song_count 
+    Song.all.length #@@song_count 
+  end 
+  #binding.pry
+end 
+#binding.pry
